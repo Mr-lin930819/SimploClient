084b6c04f5bf0a8a436837007b419a6d8d71ca6a
Mrlin930819 <946281858@qq.com>
2015-11-11 17:02:24 +0800

    添加数据库操作的java类

---
+++ Android/SimploC/app/src/main/java/com/localhost/lin/simploc/com/localhost/lin/simploc/SQLite/SQLiteOperation.java
@@ -0,0 +1,44 @@
+package com.localhost.lin.simploc.com.localhost.lin.simploc.SQLite;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by Lin on 2015/11/11.
+ */
+public class SQLiteOperation {
+    private DatabaseOpearator databaseOpearator = null;
+    public SQLiteOperation(Context context){
+        databaseOpearator = new DatabaseOpearator(context,"users.db",null,1);
+    }
+
+    public void insert(String number,String password,String cookie,String name){
+        SQLiteDatabase db = databaseOpearator.getWritableDatabase();// 取得数据库操作
+        db.execSQL("insert into userInfo (number,password,cookie,name) values(?,?,?,?)",
+                new Object[] {number,password,cookie,name });
+        db.close();// 记得关闭数据库操作
+    }
+
+    public void delete(String number) {// 删除纪录
+        SQLiteDatabase db = databaseOpearator.getWritableDatabase();
+        db.execSQL("delete from userInfo where number=?", new Object[] {number });
+        db.close();
+    }
+
+
+    public String[] find(String number) {// 根据学号查找纪录
+        String[] tusers = null;
+        SQLiteDatabase db = databaseOpearator.getReadableDatabase();
+        // 用游标Cursor接收从数据库检索到的数据
+        Cursor cursor = db.rawQuery("select * from userInfo where number=?", new String[]{number});
+        if (cursor.moveToFirst()) {// 依次取出数据
+            tusers[0] = (cursor.getString(cursor.getColumnIndex("number")));
+            tusers[1] = (cursor.getString(cursor.getColumnIndex("password")));
+            tusers[2] = (cursor.getString(cursor.getColumnIndex("cookie")));
+            tusers[3] = (cursor.getString(cursor.getColumnIndex("name")));
+        }
+        db.close();
+        return tusers;
+    }
+}
